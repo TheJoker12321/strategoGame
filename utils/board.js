59f5da80656411ff916bab30192febf4db345c89
hagay kohelet <hagay2506@gmail.com>
2025-12-07 17:48:05 +0200

    not finish

--- utils/board.js
+++ utils/board.js
@@ -10,5 +10,56 @@ export function createBoard(size = 10) {
     return board
 }
 
-let board = createBoard(3)
-console.log(board);
+export function createSoliders(size = 10) {
+    let soliders = ["flag"]
+    for (let i = 1; i < size; i++) {
+        soliders.push(i)
+    }
+    return soliders
+}
+
+export function shuffleSoliders(solidersList) {
+    let swaps = 100
+    for (let i = 0; i < swaps;) {
+        let idx1 = Math.floor(Math.random() * solidersList.length)
+        let idx2 = Math.floor(Math.random() * solidersList.length)
+        if (idx1 == idx2) {
+            continue
+        }
+        else {
+            let temp = solidersList[idx1]
+            solidersList[idx1] = solidersList[idx2]
+            solidersList[idx2] = temp
+            i++
+        }
+    } return solidersList
+}
+
+export function soldiersToBoard(board, soliders) {
+    let row = 0
+    let col = 0
+    while (soliders.length !== 0) {
+        let solider = soliders.pop()
+        board[row][col] = solider
+        col++
+        if (col === board[0].length) {
+            col = 0
+            row++
+        }
+    }
+    return board;
+}
+
+
+
+
+
+
+
+
+// let soliders = shuffleSoliders(createSoliders(20))
+// let board = createBoard()
+// let fullboard = soldiersToBoard(board, soliders)
+// console.log(fullboard);
+
+
